4398c3d32964f2d2266cb65a23448b6bd25b0b3d
hyeyoonc <lucy8624@naver.com>
2020-09-07 21:12:56 +0900

    메소드 열거

--- Car.java
+++ Car.java
@@ -6,4 +6,22 @@ public class Car {
 	int number;
 	
 
+	public Car(String name) {
+		this.name = name;
+		
+	}	
+		
+	public Car() {
+//		this.name = "이름없음";
+//		this.number = 0;
+		this("이름없음,0");
+		
+		
+	}
+	public Car(String name, int number) {
+		this.name = name;
+		this.number = number;
+		
+	}
+	
 }
